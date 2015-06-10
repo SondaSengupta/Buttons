21fd266a76c1d6616a56932f2710058bbdaecaa0
SondaSengupta <sondasengupta@gmail.com>
2015-06-09 23:39:32 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Buttons
 Lotsa buttons
+
+##Log
+This is actually just two. More to come!
---
+++ main.js
@@ -0,0 +1,9 @@
+
+function clickMyButton() {
+  document.getElementById("body").style.background = "blue";
+}
+
+function changeItBack(){
+  document.getElementById("body").style.background = "white";
+
+}
